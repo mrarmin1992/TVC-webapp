a82f22e683c2fce62603cd276e1318bc7621dff2
mrarmin <52494368+mrarmin@users.noreply.github.com>
2021-11-12 19:20:16 +0100

    dorade kategorije

--- src/app/components/admin/kategorije-proizvoda1-add/kategorije-proizvoda1-add.component.ts
+++ src/app/components/admin/kategorije-proizvoda1-add/kategorije-proizvoda1-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { KategorijaProizvoda1 } from 'src/app/models/KategorijaProizvoda1';
+import { KategorijeProizvoda1Service } from 'src/app/services/kategorije-proizvoda1.service';
 
 @Component({
   selector: 'app-kategorije-proizvoda1-add',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KategorijeProizvoda1AddComponent implements OnInit {
 
-  constructor() { }
+  kategorija: KategorijaProizvoda1 = {
+    Naziv: ''
+  };
+  constructor(private kategorije: KategorijeProizvoda1Service,
+              private router: Router,
+              private fm: FlashMessagesService) { }
 
   ngOnInit(): void {
   }
-
+  onSubmit({value, valid}: {value: KategorijaProizvoda1, valid: boolean}) {
+    if (!valid) {
+      console.log(value);
+    } else {
+      this.kategorije.addkategorijaProizvodi1(this.kategorija);
+      this.fm.show('Nova kategorija je uspješno dodana', { cssClass: 'alert-success', timeout: 3000 });
+      this.router.navigate([`dashboard/kategorije-proizvodi1`]);
+    }
+  }
 }
--- src/app/components/admin/kategorije-proizvoda2-add/kategorije-proizvoda2-add.component.ts
+++ src/app/components/admin/kategorije-proizvoda2-add/kategorije-proizvoda2-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { KategorijaProizvoda2 } from 'src/app/models/KategorijaProizvoda2';
+import { KategorijeProizvoda2Service } from 'src/app/services/kategorije-proizvoda2.service';
 
 @Component({
   selector: 'app-kategorije-proizvoda2-add',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KategorijeProizvoda2AddComponent implements OnInit {
 
-  constructor() { }
+  kategorija: KategorijaProizvoda2 = {
+    Naziv: ''
+  };
+  constructor(private kategorije: KategorijeProizvoda2Service,
+              private router: Router,
+              private fm: FlashMessagesService) { }
 
   ngOnInit(): void {
   }
-
+  onSubmit({value, valid}: {value: KategorijaProizvoda2, valid: boolean}) {
+    if (!valid) {
+      console.log(value);
+    } else {
+      this.kategorije.addkategorijaProizvodi2(this.kategorija);
+      this.fm.show('Nova kategorija je uspješno dodana', { cssClass: 'alert-success', timeout: 3000 });
+      this.router.navigate([`dashboard/kategorije-proizvodi2`]);
+    }
+  }
 }
