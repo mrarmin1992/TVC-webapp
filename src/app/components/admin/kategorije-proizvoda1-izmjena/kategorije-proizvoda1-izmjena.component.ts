648e6237824b969de9e89bce976b6529f3f92466
mrarmin <52494368+mrarmin@users.noreply.github.com>
2021-11-12 19:33:47 +0100

    dorade

--- src/app/components/admin/kategorije-proizvoda1-izmjena/kategorije-proizvoda1-izmjena.component.ts
+++ src/app/components/admin/kategorije-proizvoda1-izmjena/kategorije-proizvoda1-izmjena.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { KategorijaProizvoda1 } from 'src/app/models/KategorijaProizvoda1';
+import { KategorijeProizvoda1Service } from 'src/app/services/kategorije-proizvoda1.service';
+import { Proizvodi1Service } from 'src/app/services/proizvodi1.service';
 
 @Component({
   selector: 'app-kategorije-proizvoda1-izmjena',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kategorije-proizvoda1-izmjena.component.css']
 })
 export class KategorijeProizvoda1IzmjenaComponent implements OnInit {
-
-  constructor() { }
+  stara: string;
+  id: string;
+  category: string;
+  kategorija: KategorijaProizvoda1 = {
+    Naziv: ''
+  };
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private kategorijeService: KategorijeProizvoda1Service,
+              private fm: FlashMessagesService,
+              private proizvodiService: Proizvodi1Service) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params.id;
+    this.kategorijeService.getKategorijaProizvodi1(this.id).subscribe(kategorija => {
+      this.kategorija = kategorija;
+      this.stara = kategorija.Naziv;
+    });
   }
-
+  onSubmit({value, valid}: {value: KategorijaProizvoda1, valid: boolean}) {
+    if (!valid) {
+      console.log(valid);
+    } else {
+        this.kategorijeService.updateKategorijaProizvodi1(value, this.id);
+        this.proizvodiService.getByKategorija(this.stara).forEach(values => {
+          values.forEach(doc => {
+            this.proizvodiService.updateKategorija(doc, this.kategorija.Naziv);
+          });
+        });
+        this.fm.show('Kategorija je uspješno izmijenjena', { cssClass: 'alert-success', timeout: 3000 });
+        this.router.navigate([`/dashboard/kategorije-proizvodi1`]);
+    }
+  }
 }
+
+
--- src/app/components/admin/kategorije-proizvoda2-izmjena/kategorije-proizvoda2-izmjena.component.ts
+++ src/app/components/admin/kategorije-proizvoda2-izmjena/kategorije-proizvoda2-izmjena.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { KategorijaProizvoda2 } from 'src/app/models/KategorijaProizvoda2';
+import { KategorijeProizvoda2Service } from 'src/app/services/kategorije-proizvoda2.service';
+import { Proizvodi2Service } from 'src/app/services/proizvodi2.service';
 
 @Component({
   selector: 'app-kategorije-proizvoda2-izmjena',
@@ -6,10 +11,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kategorije-proizvoda2-izmjena.component.css']
 })
 export class KategorijeProizvoda2IzmjenaComponent implements OnInit {
-
-  constructor() { }
+  stara: string;
+  id: string;
+  category: string;
+  kategorija: KategorijaProizvoda2 = {
+    Naziv: ''
+  };
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private kategorijeService: KategorijeProizvoda2Service,
+              private fm: FlashMessagesService,
+              private proizvodiService: Proizvodi2Service) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params.id;
+    this.kategorijeService.getKategorijaProizvodi2(this.id).subscribe(kategorija => {
+      this.kategorija = kategorija;
+      this.stara = kategorija.Naziv;
+    });
   }
-
+  onSubmit({value, valid}: {value: KategorijaProizvoda2, valid: boolean}) {
+    if (!valid) {
+      console.log(valid);
+    } else {
+        this.kategorijeService.updateKategorijaProizvodi2(value, this.id);
+        this.proizvodiService.getByKategorija(this.stara).forEach(values => {
+          values.forEach(doc => {
+            this.proizvodiService.updateKategorija(doc, this.kategorija.Naziv);
+          });
+        });
+        this.fm.show('Kategorija je uspješno izmijenjena', { cssClass: 'alert-success', timeout: 3000 });
+        this.router.navigate([`/dashboard/kategorije-proizvodi2`]);
+    }
+  }
 }
+
